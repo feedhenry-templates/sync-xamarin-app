4038b2da454a1c983afbd2ea31b9f774cf55195f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-04-15 16:28:51 +0200

    update library to rc1

--- sync-android-app/SplashScreenActivity.cs
+++ sync-android-app/SplashScreenActivity.cs
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Copyright Red Hat, Inc, and individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +18,8 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Support.V7.App;
-using FHSDK.Droid;
+using FHSDK;
+using Android.Content;
 
 namespace syncandroidapp
 {
